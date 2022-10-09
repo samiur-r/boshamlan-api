d7a3771d6f07a45c2076fb0059e688b55bc62c18
samiur-r <samiurrahman.dev@gmail.com>
2022-10-10 04:18:38 +0600

    chore: add eslint airbnb style

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const config = {
-  nodeEnv: process.env['NODE_ENV'] ?? 'development',
-  port: process.env['PORT'] ?? 5000,
+  nodeEnv: process.env.NODE_ENV ?? 'development',
+  port: process.env.PORT ?? 5000,
 
   clientOrigins: {
-    development: process.env['DEV_ORIGIN'] ?? '*',
-    production: process.env['PROD_ORIGIN'] ?? 'none',
+    development: process.env.DEV_ORIGIN ?? '*',
+    production: process.env.PROD_ORIGIN ?? 'none',
   },
 };
 
