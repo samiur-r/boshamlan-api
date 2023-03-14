96add7dc7c4a505f0a7454e3c161ce085168dcc7
samiur-r <samiurrahman.dev@gmail.com>
2023-03-14 20:54:27 +0600

    chore: update env

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import app from './app';
 import config from './config';
 import logger from './utils/logger';
-import AppDataSource from './db';
-import cronJob from './utils/cornJobs';
+// import AppDataSource from './db';
+// import cronJob from './utils/cornJobs';
 
-AppDataSource.initialize()
-  .then(() => {
-    logger.info('Connected to database');
-    cronJob.start();
-    app.listen(config.port, () => {
-      logger.info(`🚀 Listening on ${config.port} with NODE_ENV=${config.nodeEnv} 🚀`);
-    });
-  })
-  .catch((error) => logger.error(`Failed to connect to database ${error}`));
+app.listen(config.port, () => {
+  logger.info(`🚀 Listening on ${config.port} with NODE_ENV=${config.nodeEnv} 🚀`);
+});
+
+// AppDataSource.initialize()
+//   .then(() => {
+//     logger.info('Connected to database');
+//     cronJob.start();
+//     app.listen(config.port, () => {
+//       logger.info(`🚀 Listening on ${config.port} with NODE_ENV=${config.nodeEnv} 🚀`);
+//     });
+//   })
+//   .catch((error) => logger.error(`Failed to connect to database ${error}`));
