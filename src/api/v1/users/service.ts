51ed6adc8d876e95746a398188c309fbc4aa284b
samiur-r <samiurrahman.dev@gmail.com>
2023-05-06 14:29:11 +0600

    test: slack notification for unverified users

--- src/api/v1/users/service.ts
+++ src/api/v1/users/service.ts
@@ -70,6 +70,7 @@ const getLastActivity = (user: any) => {
 
 const findUnVerifiedUsers = async () => {
   const lessThanFiveMins = new Date(Date.now() - 1 * 60 * 1000); // 5 minutes ago
+  console.log(new Date(), lessThanFiveMins);
   const users = await User.find({ where: { status: 'not_verified', created_at: MoreThan(lessThanFiveMins) } });
   return users;
 };
